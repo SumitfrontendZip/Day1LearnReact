6526272203be4d6a700b5c0dbe7b26f81568f383
Sumit-Kumar <sumit@frontendzip.com>
2024-05-16 17:14:54 +0530

    Learn Conditional Rendering and Lists

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -2,9 +2,14 @@ import { useState } from "react";
 
 function Counter(){
     const [count , setCount] = useState(0)
+    const [todos, setTodos] = useState(false)
     return (
         <>
-        
+            {
+                todos ?  <h1>hello</h1> : <h1>world</h1>
+
+            }
+            <button onClick={()=> setTodos(!todos)}>Change</button>
             Count : {count}
             <button onClick={()=>setCount(count+1)}>Inc</button>
             <button onClick={()=>setCount(count-1)}>Dec</button>
