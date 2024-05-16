f1b286b16d5edc4fd9fc5b4fc4e62c95076df842
Sumit-Kumar <sumit@frontendzip.com>
2024-05-16 12:26:10 +0530

    create multiples components

---
+++ src/para.jsx
@@ -0,0 +1,10 @@
+
+function Para() { // Corrected component name
+  return (
+    <>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore recusandae itaque eligendi expedita ipsam sit enim natus officia magnam eaque sequi tenetur rerum, dolorem nobis rem tempore obcaecati, non quibusdam ea repellendus eius? Voluptates amet doloremque placeat minima? Tempora quis perspiciatis sed corrupti, autem fugit neque repudiandae accusamus ipsa perferendis asperiores excepturi corporis distinctio, maiores consequatur ut! Laudantium adipisci reprehenderit sit, laborum dolore exercitationem sapiente impedit qui sunt provident aliquid consectetur nostrum, quam, accusantium corporis sequi maxime assumenda non! Veniam placeat dolor aliquid perferendis voluptatum est ipsum amet saepe magni accusamus, quisquam ipsa quo! Quaerat minus architecto recusandae magni laboriosam.
+    </>
+  );
+}
+
+export default Para; // Corrected export
